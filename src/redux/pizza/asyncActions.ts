ecc2e27487307aa7952d4bbb5685fd1138fd01d8
Mykhailo <111697550+kozmamisha@users.noreply.github.com>
2023-06-20 15:40:48 +0300

    Update asyncActions.ts

--- src/redux/pizza/asyncActions.ts
+++ src/redux/pizza/asyncActions.ts
@@ -7,7 +7,7 @@ export const fetchPizzas = createAsyncThunk<Pizza[], SearchPizzaParams>(
   async (params) => {
     const { sortBy, order, category, search, currentPage } = params;
     const { data } = await axios.get(
-      `https://run.mocky.io/v3/bac8d08f-2c22-4be4-9fd4-c86815b306d2?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+      `https://64919c042f2c7ee6c2c894e9.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
     );
 
     return data;
